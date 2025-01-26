5bd43d202c320071cc40ea10420d011062f0abaa
konika098 <konikakhan2000@gmail.com>
2025-01-26 18:35:47 +0600

    make it responsive

--- src/Components/Layout/NavBar/SearchBar.tsx
+++ src/Components/Layout/NavBar/SearchBar.tsx
@@ -1,72 +1,56 @@
-import type React from "react";
-import { useState, useRef, useEffect, FC } from "react";
+import type React from "react"
+import { useState, useRef, useEffect, type FC } from "react"
 
 interface SearchBarProps {
-  onSearchStateChange: (isExpanded: boolean) => void;
-  themeMode?: boolean;
+  onSearchStateChange: (isExpanded: boolean) => void
+  themeMode?: boolean
+  isSmallDevice: boolean
 }
 
-const SearchBar: FC<SearchBarProps> = ({ onSearchStateChange, themeMode }) => {
-  const [searchText, setSearchText] = useState<string>("");
-  const [showDropdown, setShowDropdown] = useState<boolean>(false);
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
-  const searchBarRef = useRef<HTMLDivElement | null>(null);
-  const options: string[] = [
-    "Natasha Djalrio",
-    "John Doe",
-    "Jane Smith",
-    "Michael Brown",
-    "Sarah Wilson",
-  ];
+const SearchBar: FC<SearchBarProps> = ({ onSearchStateChange, themeMode, isSmallDevice }) => {
+  const [searchText, setSearchText] = useState<string>("")
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
+  const [isExpanded, setIsExpanded] = useState<boolean>(false)
+  const searchBarRef = useRef<HTMLDivElement | null>(null)
+  const options: string[] = ["Natasha Djalrio", "John Doe", "Jane Smith", "Michael Brown", "Sarah Wilson"]
 
   const handleSearchClick = () => {
-    const newExpandedState = !isExpanded;
-    setShowDropdown(newExpandedState);
-    setIsExpanded(newExpandedState);
-    onSearchStateChange(newExpandedState);
-  };
+    const newExpandedState = !isExpanded
+    setShowDropdown(newExpandedState)
+    setIsExpanded(newExpandedState)
+    onSearchStateChange(newExpandedState)
+  }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value);
-  };
+    setSearchText(e.target.value)
+  }
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (
-        searchBarRef.current &&
-        !searchBarRef.current.contains(event.target as Node)
-      ) {
-        setShowDropdown(false);
-        setIsExpanded(false);
-        onSearchStateChange(false);
+      if (searchBarRef.current && !searchBarRef.current.contains(event.target as Node)) {
+        setShowDropdown(false)
+        setIsExpanded(false)
+        onSearchStateChange(false)
       }
-    };
+    }
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside)
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [onSearchStateChange]);
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [onSearchStateChange])
 
   return (
     <div
-      className={`relative transition-all duration-300 ease-in-out ${isExpanded ? "w-full" : "w-[482px]"}`}
+      className={`relative transition-all duration-300 ease-in-out ${
+        isExpanded ? "w-full" : isSmallDevice ? "w-10" : "w-[482px]"
+      }`}
       ref={searchBarRef}
     >
-      <div
-        className={`border border-[#BBBCC0]  py-3 pl-4 pr-3 rounded-md bg-white `}
-      >
-        <div className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={22}
-            height={22}
-            viewBox="0 0 22 22"
-            fill="none"
-            className="cursor-pointer"
-            onClick={handleSearchClick}
-          >
+      {isSmallDevice && !isExpanded ? (
+        <div className="cursor-pointer flex items-center justify-center w-10 h-10" onClick={handleSearchClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 22 22" fill="none">
             <path
               d="M12.8334 4.5835H18.3334"
               stroke="#BBBCC0"
@@ -96,60 +80,95 @@ const SearchBar: FC<SearchBarProps> = ({ onSearchStateChange, themeMode }) => {
               strokeLinejoin="round"
             />
           </svg>
-          <input
-            className="ml-2.5 w-full h-full bg-transparent text-gray-700 font-sans font-normal outline-none"
-            placeholder="Search Anything"
-            value={searchText}
-            onChange={handleInputChange}
-            onFocus={() => {
-              setShowDropdown(true);
-              setIsExpanded(true);
-              onSearchStateChange(true);
-            }}
-          />
-          {isExpanded && (
-            <button
-              onClick={() => {
-                setIsExpanded(false);
-                setShowDropdown(false);
-                setSearchText("");
-                onSearchStateChange(false);
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="27"
-                height="26"
-                viewBox="0 0 27 26"
-                fill="none"
-              >
-                <path
-                  d="M6.61914 19.5L19.859 6.5M6.61914 6.5L19.859 19.5"
-                  stroke="#6D6E76"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </button>
-          )}
         </div>
-      </div>
+      ) : (
+        <div className={`border border-[#BBBCC0]  py-3 pl-4 pr-3 rounded-md bg-white `}>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={22}
+              height={22}
+              viewBox="0 0 22 22"
+              fill="none"
+              className="cursor-pointer"
+              onClick={handleSearchClick}
+            >
+              <path
+                d="M12.8334 4.5835H18.3334"
+                stroke="#BBBCC0"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M12.8334 7.3335H15.5834"
+                stroke="#BBBCC0"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M19.25 10.5418C19.25 15.3543 15.3542 19.2502 10.5417 19.2502C5.72921 19.2502 1.83337 15.3543 1.83337 10.5418C1.83337 5.72933 5.72921 1.8335 10.5417 1.8335"
+                stroke="#BBBCC0"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M20.1667 20.1668L18.3334 18.3335"
+                stroke="#BBBCC0"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input
+              className="ml-2.5 w-full h-full bg-transparent text-gray-700 font-sans font-normal outline-none"
+              placeholder="Search Anything"
+              value={searchText}
+              onChange={handleInputChange}
+              onFocus={() => {
+                setShowDropdown(true)
+                setIsExpanded(true)
+                onSearchStateChange(true)
+              }}
+            />
+            {isExpanded && (
+              <button
+                onClick={() => {
+                  setIsExpanded(false)
+                  setShowDropdown(false)
+                  setSearchText("")
+                  onSearchStateChange(false)
+                }}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="27" height="26" viewBox="0 0 27 26" fill="none">
+                  <path
+                    d="M6.61914 19.5L19.859 6.5M6.61914 6.5L19.859 19.5"
+                    stroke="#6D6E76"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
+            )}
+          </div>
+        </div>
+      )}
       {showDropdown && (
         <ul className="absolute z-10 mt-1 w-full border border-gray-300 bg-white rounded-md shadow-lg max-h-60 overflow-y-auto">
           {options
-            .filter((option) =>
-              option.toLowerCase().includes(searchText.toLowerCase())
-            )
+            .filter((option) => option.toLowerCase().includes(searchText.toLowerCase()))
             .map((option, index) => (
               <li
                 key={index}
                 className="flex justify-between items-center px-4 py-2 hover:bg-gray-100 cursor-pointer"
                 onClick={() => {
-                  setSearchText(option);
-                  setShowDropdown(false);
-                  setIsExpanded(false);
-                  onSearchStateChange(false);
+                  setSearchText(option)
+                  setShowDropdown(false)
+                  setIsExpanded(false)
+                  onSearchStateChange(false)
                 }}
               >
                 <div className="flex items-center">
@@ -192,13 +211,7 @@ const SearchBar: FC<SearchBarProps> = ({ onSearchStateChange, themeMode }) => {
                   </svg>
                   {option}
                 </div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="26"
-                  height="26"
-                  viewBox="0 0 26 26"
-                  fill="none"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
                   <path
                     d="M5.41602 3.79199L20.5827 18.9587"
                     stroke="#6D6E76"
@@ -221,7 +234,8 @@ const SearchBar: FC<SearchBarProps> = ({ onSearchStateChange, themeMode }) => {
         </ul>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
+
--- src/Components/Layout/NavBar/index.tsx
+++ src/Components/Layout/NavBar/index.tsx
@@ -1,124 +1,126 @@
-import type React from "react";
-import { Fragment, useEffect, useState } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import { ReactComponent as SummaryIcon } from "../../../assets/svg/summaryIcon.svg";
-import { ReactComponent as FacebookIcon } from "../../../assets/svg/facebookIcon.svg";
-import { ReactComponent as YoutubeIcon } from "../../../assets/svg/youtubeIcon.svg";
-import { ReactComponent as MobileMenuIcon } from "../../../assets/svg/mobileMenuIcon.svg";
-import { Modal, ModalOverlay, ModalContent } from "@chakra-ui/react";
-import { AiOutlineClose } from "react-icons/ai";
-import * as common from "../../../Constant/constants";
-import "./style.css";
-import BreadCrumb from "../../BreadCrumb";
-import { PoznanskiLogoIcon } from "../../../assets/svg/poznanskiLogo";
-import { AccountMenu } from "../../AccountMenu/AccountMenu";
-import { ReactComponent as InstagramIcon } from "../../../assets/svg/instagramIcon.svg";
-import { DelayedLink } from "../../_utility/DelayedLink";
-import { ActionButton } from "../../Button";
-import SearchBar from "./SearchBar";
+import type React from "react"
+import { Fragment, useEffect, useState } from "react"
+import { Link, useNavigate, useLocation } from "react-router-dom"
+import { ReactComponent as SummaryIcon } from "../../../assets/svg/summaryIcon.svg"
+import { ReactComponent as FacebookIcon } from "../../../assets/svg/facebookIcon.svg"
+import { ReactComponent as YoutubeIcon } from "../../../assets/svg/youtubeIcon.svg"
+import { ReactComponent as MobileMenuIcon } from "../../../assets/svg/mobileMenuIcon.svg"
+import { Modal, ModalOverlay, ModalContent } from "@chakra-ui/react"
+import { AiOutlineClose } from "react-icons/ai"
+import * as common from "../../../Constant/constants"
+import "./style.css"
+import BreadCrumb from "../../BreadCrumb"
+import { PoznanskiLogoIcon } from "../../../assets/svg/poznanskiLogo"
+import { AccountMenu } from "../../AccountMenu/AccountMenu"
+import { ReactComponent as InstagramIcon } from "../../../assets/svg/instagramIcon.svg"
+import { DelayedLink } from "../../_utility/DelayedLink"
+import { ActionButton } from "../../Button"
+import SearchBar from "./SearchBar"
 
 interface NavBarProps {
-  filterText?: string;
-  setFilterText?: React.Dispatch<React.SetStateAction<string>>;
-  themeMode?: boolean;
-  type?: boolean;
+  filterText?: string
+  setFilterText?: React.Dispatch<React.SetStateAction<string>>
+  themeMode?: boolean
+  type?: boolean
 }
 
-const menu = [
-  "Newsy",
-  "TV/Radio",
-  "Nasze materiały",
-  "Albumy",
-  "Artyści",
-  "Wydarzenia",
-];
+const menu = ["Newsy", "TV/Radio", "Nasze materiały", "Albumy", "Artyści", "Wydarzenia"]
 
-export const getIconsColor = (themeMode?: boolean) =>
-  themeMode ? "#5A1073" : "#2FC4B2";
+export const getIconsColor = (themeMode?: boolean) => (themeMode ? "#5A1073" : "#2FC4B2")
 
 const NavBar: React.FC<NavBarProps> = (props) => {
-  const [selectedMenu, setSelectedMenu] = useState<string>("Home");
-  const [openFilterBox, setOpenFilterBox] = useState<boolean>(false);
-  const [openModal, setOpenModal] = useState<boolean>(false);
-  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
+  const [selectedMenu, setSelectedMenu] = useState<string>("Home")
+  const [openFilterBox, setOpenFilterBox] = useState<boolean>(false)
+  const [openModal, setOpenModal] = useState<boolean>(false)
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false)
+  const [isSmallDevice, setIsSmallDevice] = useState(false) // Added state for small device
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    const checkDeviceSize = () => {
+      setIsSmallDevice(window.innerWidth < 768) // Adjust the breakpoint as needed
+    }
+
+    checkDeviceSize()
+    window.addEventListener("resize", checkDeviceSize)
+
+    return () => window.removeEventListener("resize", checkDeviceSize)
+  }, []) // Added useEffect to check device size
 
   useEffect(() => {
     switch (true) {
       case location.pathname.includes(common.TV_RADIO_PATH):
-        setSelectedMenu("TV/Radio");
-        break;
+        setSelectedMenu("TV/Radio")
+        break
       case location.pathname.includes(common.CONCERT_PATH):
-        setSelectedMenu("Wydarzenia");
-        break;
+        setSelectedMenu("Wydarzenia")
+        break
       case location.pathname.includes(common.NEWS_PATH):
-        setSelectedMenu("Newsy");
-        break;
+        setSelectedMenu("Newsy")
+        break
       case location.pathname.includes(common.ARTISTS_PATH):
-        setSelectedMenu("Artyści");
-        break;
+        setSelectedMenu("Artyści")
+        break
       case location.pathname.includes(common.MATERIAL_PATH):
-        setSelectedMenu("Nasze materiały");
-        break;
+        setSelectedMenu("Nasze materiały")
+        break
       case location.pathname.includes(common.NEWRELEASE_PATH):
-        setSelectedMenu("Albumy");
-        break;
+        setSelectedMenu("Albumy")
+        break
       default:
-        setSelectedMenu("Home");
-        break;
+        setSelectedMenu("Home")
+        break
     }
-  }, [location.pathname]);
+  }, [location.pathname])
 
   const onClick = (value: string): void => {
-    setSelectedMenu(value);
+    setSelectedMenu(value)
     switch (value) {
       case "Home":
-        navigate(common.HOME_PATH);
-        break;
+        navigate(common.HOME_PATH)
+        break
       case "TV/Radio":
-        navigate(common.TV_RADIO_PATH);
-        break;
+        navigate(common.TV_RADIO_PATH)
+        break
       case "Wydarzenia":
-        navigate(common.CONCERT_PATH);
-        break;
+        navigate(common.CONCERT_PATH)
+        break
       case "Newsy":
-        navigate(common.NEWS_PATH);
-        break;
+        navigate(common.NEWS_PATH)
+        break
       case "Artyści":
-        navigate(common.ARTISTS_PATH);
-        break;
+        navigate(common.ARTISTS_PATH)
+        break
       case "Nasze materiały":
-        navigate(common.MATERIAL_PATH);
-        break;
+        navigate(common.MATERIAL_PATH)
+        break
       case "Albumy":
-        navigate(common.NEWRELEASE_PATH);
-        break;
+        navigate(common.NEWRELEASE_PATH)
+        break
       default:
-        break;
+        break
     }
     setTimeout(() => {
-      setOpenModal(false);
-    }, 200);
-  };
+      setOpenModal(false)
+    }, 200)
+  }
 
   const onClose = (): void => {
-    setOpenModal(false);
-  };
+    setOpenModal(false)
+  }
 
   const handleSearchStateChange = (expanded: boolean) => {
-    setIsSearchExpanded(expanded);
-  };
+    setIsSearchExpanded(expanded)
+  }
 
   return (
     <Fragment>
       <div className={`Nav-bar fixed w-full z-50 shadow-xl`}>
-        <div
-          className={`Nav-bar-top ${!props.themeMode && "Nav-bar-top-dark"} flex place-items-center justify-center`}
-        >
+        <div className={`Nav-bar-top ${!props.themeMode && "Nav-bar-top-dark"} flex place-items-center justify-center`}>
           <div className="flex justify-between gap-x-1 container py-4 px-4 md:px-6">
             <div className="flex w-full place-items-center">
-              <Link to={"/"} className={isSearchExpanded ? "hidden" : ""}>
+              <Link to={"/"} className={isSearchExpanded && (isSmallDevice || !isSmallDevice) ? "hidden" : ""}>
                 <PoznanskiLogoIcon
                   className="mr-4 rounded-full shadow-2xl w-10 h-10 md:w-12 md:h-12"
                   fill={props.themeMode ? "#000" : "#fff"}
@@ -127,15 +129,13 @@ const NavBar: React.FC<NavBarProps> = (props) => {
               <SearchBar
                 onSearchStateChange={handleSearchStateChange}
                 themeMode={true}
+                isSmallDevice={isSmallDevice} 
               />
               <button
                 onClick={() => setOpenModal(true)}
-                className="md:hidden flex items-center justify-center w-10 h-10"
+                className={`md:hidden flex items-center justify-center w-10 h-10 ${isSearchExpanded ? "hidden" : ""}`}
               >
-                <MobileMenuIcon
-                  className="w-6 h-6"
-                  stroke={getIconsColor(props.themeMode)}
-                />
+                <MobileMenuIcon className="w-6 h-6" stroke={getIconsColor(props.themeMode)} />
               </button>
             </div>
 
@@ -143,22 +143,10 @@ const NavBar: React.FC<NavBarProps> = (props) => {
               <div className="hidden md:block w-full">
                 <div className="flex-2 flex place-items-center gap-x-4 h-full justify-end">
                   <div className="grid grid-cols-4 gap-x-4">
-                    <YoutubeIcon
-                      className="cursor-pointer"
-                      stroke={getIconsColor(props.themeMode)}
-                    />
-                    <FacebookIcon
-                      className="cursor-pointer"
-                      stroke={getIconsColor(props.themeMode)}
-                    />
-                    <InstagramIcon
-                      className="cursor-pointer"
-                      stroke={getIconsColor(props.themeMode)}
-                    />
-                    <SummaryIcon
-                      className="cursor-pointer"
-                      stroke={getIconsColor(props.themeMode)}
-                    />
+                    <YoutubeIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
+                    <FacebookIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
+                    <InstagramIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
+                    <SummaryIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
                   </div>
                   <AccountMenu themeMode={props.themeMode} />
                   <Link to={common.CREATE_NEWS}>
@@ -180,18 +168,13 @@ const NavBar: React.FC<NavBarProps> = (props) => {
         </div>
 
         {props.type || (
-          <div
-            className={`Nav-bar-down ${
-              !props.themeMode && "Nav-bar-down-dark"
-            } space-around`}
-          >
+          <div className={`Nav-bar-down ${!props.themeMode && "Nav-bar-down-dark"} space-around`}>
             <div className="flex justify-center gap-10">
               {menu.map((item, idx) => (
                 <div
                   key={`nav-btn-${idx}`}
                   className={`Nav-btn ${!props.themeMode && "Nav-btn-dark"} ${
-                    item === selectedMenu &&
-                    (props.themeMode ? "selected-menu" : "selected-menu-dark")
+                    item === selectedMenu && (props.themeMode ? "selected-menu" : "selected-menu-dark")
                   }`}
                   onClick={() => onClick(item)}
                 >
@@ -204,23 +187,15 @@ const NavBar: React.FC<NavBarProps> = (props) => {
       </div>
       <Modal onClose={onClose} size="full" isOpen={openModal}>
         <ModalOverlay />
-        <ModalContent
-          className={`container ${!props.themeMode && "back-dark"} justify-between h-screen p-4 md:p-6`}
-        >
+        <ModalContent className={`container ${!props.themeMode && "back-dark"} justify-between h-screen p-4 md:p-6`}>
           <div className="flex-col h-full">
             <div className="flex justify-between my-4">
               <div className="cursor-pointer">
-                <PoznanskiLogoIcon
-                  className="rounded-full shadow-2xl"
-                  fill={props.themeMode ? "#000" : "#fff"}
-                />
+                <PoznanskiLogoIcon className="rounded-full shadow-2xl" fill={props.themeMode ? "#000" : "#fff"} />
               </div>
 
               <div className="flex items-center" onClick={onClose}>
-                <AiOutlineClose
-                  size={24}
-                  color={props.themeMode ? "" : "#FFF"}
-                />
+                <AiOutlineClose size={24} color={props.themeMode ? "" : "#FFF"} />
               </div>
             </div>
             <div className="flex justify-center mb-10">
@@ -231,8 +206,7 @@ const NavBar: React.FC<NavBarProps> = (props) => {
                 <div
                   key={`nav-vertical-btn-${idx}`}
                   className={`Nav-vertical-btn text-center ${
-                    item === selectedMenu &&
-                    (props.themeMode ? "selected-menu" : "text-dark-color")
+                    item === selectedMenu && (props.themeMode ? "selected-menu" : "text-dark-color")
                   } h-[15%] content-center`}
                   onClick={() => onClick(item)}
                 >
@@ -242,29 +216,18 @@ const NavBar: React.FC<NavBarProps> = (props) => {
             </div>
             <div className="flex justify-center">
               <div className="grid grid-cols-4 gap-x-8 -mb-20">
-                <YoutubeIcon
-                  className="cursor-pointer"
-                  stroke={getIconsColor(props.themeMode)}
-                />
-                <FacebookIcon
-                  className="cursor-pointer"
-                  stroke={getIconsColor(props.themeMode)}
-                />
-                <InstagramIcon
-                  className="cursor-pointer"
-                  stroke={getIconsColor(props.themeMode)}
-                />
-                <SummaryIcon
-                  className="cursor-pointer"
-                  stroke={getIconsColor(props.themeMode)}
-                />
+                <YoutubeIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
+                <FacebookIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
+                <InstagramIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
+                <SummaryIcon className="cursor-pointer" stroke={getIconsColor(props.themeMode)} />
               </div>
             </div>
           </div>
         </ModalContent>
       </Modal>
     </Fragment>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
+
